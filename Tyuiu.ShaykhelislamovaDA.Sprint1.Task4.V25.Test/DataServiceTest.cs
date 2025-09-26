85e808901711c5998309d5f389dc183fe1aa903a
Диана Шайхелисламова <Tyuiu.ShaykhelislamovaDA@outlook.com>
2025-09-26 13:29:38 +0500

    Task4

---
+++ Tyuiu.ShaykhelislamovaDA.Sprint1.Task4.V25.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.ShaykhelislamovaDA.Sprint1.Task4.V25.Lib;
+namespace Tyuiu.ShaykhelislamovaDA.Sprint1.Task4.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest 
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 90;
+            var res = ds.Calculate(x);
+            Assert.AreEqual(1.812, res);
+        }
+    }
+}
---
+++ Tyuiu.ShaykhelislamovaDA.Sprint1.Task4.V25/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.ShaykhelislamovaDA.Sprint1.Task4.V25.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выолнила: Шайхелисламова Д. А. | ПКТБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Class Math                                                              *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #25                                                             *");
+        Console.WriteLine("* Выполнила: Шайхелисламова Диана Артуровна | ПКТБ-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* вычисляет результат по формуле (1 - cos(a)) / (sin(a))^2 и печатает его *");
+        Console.WriteLine("* на экране.                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double a;
+        Console.WriteLine("");
+        a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.Calculate(a));
+
+
+    }
+}
