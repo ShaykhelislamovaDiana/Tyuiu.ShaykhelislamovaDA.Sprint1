e6291b3f4aadc75f6cbbacebdb2f47afbd0cc6bb
Диана Шайхелисламова <Tyuiu.ShaykhelislamovaDA@outlook.com>
2025-09-26 15:37:39 +0500

    Task6

---
+++ Tyuiu.ShaykhelislamovaDA.Sprint1.Task6.V2.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ShaykhelislamovaDA.Sprint1.Task6.V2.Lib;
+namespace Tyuiu.ShaykhelislamovaDA.Sprint1.Task6.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            string strTest = "Hellooo!";
+            DataService ds = new DataService();
+            bool res = ds.CheckHello(strTest);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ShaykhelislamovaDA.Sprint1.Task6.V2/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.ShaykhelislamovaDA.Sprint1.Task6.V2.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выолнила: Шайхелисламова Д. А. | ПКТБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Работа со строками класс String                                         *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #2                                                              *");
+        Console.WriteLine("* Выполнила: Шайхелисламова Диана Артуровна | ПКТБ-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу: пользователь вводит текст. Проверить, есть и в      *");
+        Console.WriteLine("* строке слово Hello.                                                     *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Напишите сообщение:");
+        string a = Console.ReadLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.CheckHello(a));
+
+    }
+}
